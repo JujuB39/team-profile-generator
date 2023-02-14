5ff1fa7dc12fa4d9bb74fe0ebaf9873b6cc0d0b5
Julissa Bonilla <julissab9@gmail.com>
2023-02-14 10:42:02 -0600

    updating engineer test, working on intern

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,13 +1,49 @@
-//Needed 
-//name
-//id
-//email
-//getName()
-//getId()
-//getEmail()
-//get role 
-//github username 
-//getGithub()
+const Engineer= require('../lib/Engineer')
+
+//My test example 
+const name = "Juju";
+const id = "3";
+const email ="julissab9@gmail.com";
+const github = "jujub39"
+
+//push my example into engineer
+const engineer = new Engineer(name, id, email, github);
+
+describe('Enigneer', () => {
+    it("Returns an Engineer object with a name, id, email, and github", () => {
+        expect("name" in engineer).toEqual(true);
+        expect('id' in engineer).toEqual(true);
+        expect("email" in engineer).toEqual(true);
+        expect("github" in engineer).toEqual(true)
+    });
+
+    it("Returns the correct name, id, email, and github user", () => {
+        expect(engineer.name).toEqual(name);
+        expect(engineer.id).toEqual(id);
+        expect(engineer.email).toEqual(email);
+        expect(engineer.github).toEqual(github);
+    })
+
+    it("Returns the correct name when getName() is called", () => {
+        expect(engineer.getName().toEqual(name))
+    })
+
+    it("Returns the correct ID when getId() is called", () => {
+        expect(engineer.getId().toEqual(id))
+    })
+
+    it("Returns the correct email when getEmail() is called", () => {
+        expect(engineer.getEmail().toEqual(email))
+    })
+
+    it("Returns the correct role when getRole() is called", () => {
+        expect(engineer.getRole().toEqual("Engineer"))
+    })
+
+    it("Returns the correct github user when getGithub() is called", () => {
+        expect(engineer.getGithub().toEqual(github))
+    })
 
 
-//other classes extends Employee 
+
+});
